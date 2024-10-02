02e07d95618ee0896967d212ca163b75a5754790
Shoaib-Programmer <shoaibnigam422@gmail.com>
2024-10-02 16:00:52 +0530

    Update titration_curves.py

--- titration_curves.py
+++ titration_curves.py
@@ -1,3 +1,5 @@
+# This program implements plotting of titration curves
+
 import matplotlib.pyplot as plt
 import numpy as np
 from .chemicals import Chemical, Acid, Base
@@ -5,3 +7,5 @@ from .titration_calculation import volume_of_titrant, Analyte, Titrant
 
 analyte = Analyte()
 titrant = Titrant()
+
+... # More is to be added
