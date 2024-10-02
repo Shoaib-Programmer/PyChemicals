eb450c82fd4cd28d2a6049dc64c0f9d806cd93d3
Shoaib-Programmer <shoaibnigam422@gmail.com>
2024-10-02 15:59:36 +0530

    Add files via upload

---
+++ titration_curves.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from .chemicals import Chemical, Acid, Base
+from .titration_calculation import volume_of_titrant, Analyte, Titrant
+
+analyte = Analyte()
+titrant = Titrant()
